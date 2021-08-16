c2e84be77b8ea9121a84aa3120b7e7448253de57
evuraan <evuraan@gmail.com>
2021-08-15 21:31:29 -0700

    Ver Swipe/2.01c: x11 and wayland compatible

---
+++ getDev.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const (
+	devices  = "/proc/bus/input/devices"
+	touchpad = "TouchPad"
+	keyboard = "keyboard"
+)
+
+var (
+	touchPadIdentifier = ""
+)
+
+// func main() {
+// 	fmt.Println("Hello!")
+// 	x := getDeviceForPattern("Integrated")
+// 	fmt.Printf("x: %#v\n", x)
+// }
+
+func getDeviceForPattern(lookForA string) (devicePath string) {
+	if len(lookForA) < 1 {
+		return
+	}
+	lookFor := strings.ToLower(lookForA)
+	fmap := parseFileToMap(devices)
+	if len(fmap) < 1 {
+		return
+	}
+	var entry, lentr string
+	pat := regexp.MustCompile(`event\d*`)
+	for k := range fmap {
+		entry = fmap[k]
+		if len(entry) < 1 {
+			continue
+		}
+		lentr = strings.ToLower(entry)
+		if strings.Contains(lentr, lookFor) {
+			pos := k + 4
+			if len(fmap) < pos {
+				continue
+			}
+			handler := fmap[pos]
+			s := pat.FindString(handler)
+
+			if len(s) > 0 {
+				devicePath = fmt.Sprintf("/dev/input/%s", s)
+				return devicePath
+			}
+		}
+	}
+
+	return devicePath
+}
+
+func parseFileToMap(configFile string) (someDict map[int]string) {
+	someDict = make(map[int]string)
+
+	func() {
+		f, err := os.Open(configFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not open config file: %v\n", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		fscanner := bufio.NewScanner(f)
+		for fscanner.Scan() {
+			line := fscanner.Text()
+			if len(line) > 0 {
+				someDict[len(someDict)] = line
+			}
+		}
+	}()
+
+	return someDict
+}
